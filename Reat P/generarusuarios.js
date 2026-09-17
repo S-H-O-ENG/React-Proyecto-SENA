d2326c8bf2ffcd70f7a4c3e89a935bbc3ba7d62b
S-H-O-ENG <sebastianherreraobando.alone@gmail.com>
2026-09-16 21:09:52 -0500

    1

---
+++ Reat P/generarusuarios.js
@@ -0,0 +1,30 @@
+import bcrypt from 'bcryptjs';
+import fs from 'fs';
+
+
+const usuariosNativos = [
+  { id: "1", nombre: "Jefe", email: "jefe@betancourt.com", role: "admin", passwordPlana: "12345" }
+];
+
+
+const usuariosEncriptados = usuariosNativos.map((usuario) => {
+  const salt = bcrypt.genSaltSync(10);
+  const passwordHash = bcrypt.hashSync(usuario.passwordPlana, salt);
+
+  return {
+    id: usuario.id,
+    nombre: usuario.nombre,
+    email: usuario.email,
+    role: usuario.role,
+    password: passwordHash 
+  };
+});
+
+
+const dataBase = {
+  usuarios: usuariosEncriptados
+};
+
+fs.writeFileSync('db.json', JSON.stringify(dataBase, null, 2));
+
+console.log('¡Base de datos db.json generada con éxito y contraseñas encriptadas!');
